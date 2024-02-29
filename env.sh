3a0a6b717e755fe99e8e08f955f53fdadc3afe23
Param Mehta <61198990+param-mehta@users.noreply.github.com>
2024-02-28 16:35:03 -0800

    Add files via upload

---
+++ env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export USER_ID_1="placeholder_user_id_1"
+export API_KEY_1="placeholder_api_key_1"
+export API_SECRET_1="placeholder_api_secret_1"
+export PASSWORD_1="placeholder_password_1"
+export TOTP_KEY_1="placeholder_totp_key_1"
